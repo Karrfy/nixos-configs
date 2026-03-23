57ea56edc3c8e9e06679267ad97dcf6627712edb
Jannis <git@jannis.one>
2026-03-23 21:42:30 +0100

    fix(de): use noctalia flake

--- src/config/home-manager/modules/de/niri.nix
+++ src/config/home-manager/modules/de/niri.nix
@@ -7,6 +7,10 @@
 }:
 
 {
+  imports = [
+    inputs.noctalia.homeModules.default
+  ];
+
   options = {
     home-configurations.de.niri = {
       enable = lib.mkEnableOption {
@@ -19,5 +23,57 @@
   config = lib.mkIf config.home-configurations.de.niri.enable {
     # Set configuration file for niri
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
+
+    programs.noctalia-shell = {
+      enable = true;
+      settings = {
+        # configure noctalia here
+        bar = {
+          density = "compact";
+          position = "right";
+          showCapsule = false;
+          widgets = {
+            left = [
+              {
+                id = "ControlCenter";
+                useDistroLogo = true;
+              }
+              {
+                id = "Network";
+              }
+              {
+                id = "Bluetooth";
+              }
+            ];
+            center = [
+              {
+                hideUnoccupied = false;
+                id = "Workspace";
+                labelMode = "none";
+              }
+            ];
+            right = [
+              {
+                alwaysShowPercentage = false;
+                id = "Battery";
+                warningThreshold = 30;
+              }
+              {
+                formatHorizontal = "HH:mm";
+                formatVertical = "HH mm";
+                id = "Clock";
+                useMonospacedFont = true;
+                usePrimaryColor = true;
+              }
+            ];
+          };
+        };
+        colorSchemes.predefinedScheme = "Monochrome";
+        location = {
+          monthBeforeDay = false;
+          name = "Berlin, Germany";
+        };
+      };
+    };
   };
 }
--- src/config/modules/de/niri.nix
+++ src/config/modules/de/niri.nix
@@ -43,58 +43,6 @@
       xwayland-satellite
     ];
 
-    programs.noctalia-shell = {
-      enable = true;
-      settings = {
-        # configure noctalia here
-        bar = {
-          density = "compact";
-          position = "right";
-          showCapsule = false;
-          widgets = {
-            left = [
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
-              {
-                id = "Network";
-              }
-              {
-                id = "Bluetooth";
-              }
-            ];
-            center = [
-              {
-                hideUnoccupied = false;
-                id = "Workspace";
-                labelMode = "none";
-              }
-            ];
-            right = [
-              {
-                alwaysShowPercentage = false;
-                id = "Battery";
-                warningThreshold = 30;
-              }
-              {
-                formatHorizontal = "HH:mm";
-                formatVertical = "HH mm";
-                id = "Clock";
-                useMonospacedFont = true;
-                usePrimaryColor = true;
-              }
-            ];
-          };
-        };
-        colorSchemes.predefinedScheme = "Monochrome";
-        location = {
-          monthBeforeDay = false;
-          name = "Berlin, Germany";
-        };
-      };
-    };
-
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
   };
 }
