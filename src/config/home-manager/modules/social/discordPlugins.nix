da526c5c6da2a01fc04f277cdbb813224b6352e2
Jannis <git@jannis.one>
2026-05-12 22:28:58 +0200

    chore(voicechat): move discord plugins into seperate file

---
+++ src/config/home-manager/modules/social/discordPlugins.nix
@@ -0,0 +1,115 @@
+{
+  anonymiseFileNames = {
+    enable = true;
+    anonymiseByDefault = true;
+  };
+  betterSessions.enable = true;
+  biggerStreamPreview.enable = true;
+  BlurNSFW.enable = true;
+  ClearURLs.enable = true;
+  consoleJanitor.enable = true;
+  copyFileContents.enable = true;
+  crashHandler = {
+    enable = true;
+    attemptToPreventCrashes = true;
+  };
+  disableCallIdle.enable = true;
+  fakeNitro = {
+    enable = true;
+    enableEmojiBypass = true;
+    enableStickerBypass = true;
+    enableStreamQualityBypass = true;
+  };
+  fixCodeblockGap.enable = true;
+  fixSpotifyEmbeds.enable = true;
+  fixYoutubeEmbeds.enable = true;
+  friendsSince.enable = true;
+  gameActivityToggle = {
+    enable = true;
+    location = "PANEL";
+    oldIcon = true;
+  };
+  imageFilename.enable = true;
+  imageZoom = {
+    enable = true;
+    invertScroll = true;
+    size = 275.0;
+    square = true;
+    zoom = 2.8;
+    zoomSpeed = 6.3;
+  };
+  messageLogger = {
+    enable = true;
+    collapseDeleted = false;
+    deleteStyle = "text";
+    ignoreBots = false;
+    ignoreSelf = false;
+    ignoreSelfEdits = false;
+    inlineEdits = true;
+    logDeletes = true;
+    logEdits = true;
+  };
+  newPluginsManager.enable = true;
+  noDevtoolsWarning.enable = true;
+  noF1.enable = true;
+  noNitroUpsell.enable = true;
+  noOnboardingDelay.enable = true;
+  noReplyMention = {
+    enable = true;
+    shouldPingListed = false;
+  };
+  openInApp = {
+    enable = true;
+    spotify = true;
+    itunes = true;
+    tidal = true;
+  };
+  platformIndicators = {
+    enable = true;
+    colorMobileIndicator = true;
+    list = true;
+    messages = false;
+    profiles = true;
+    showBots = false;
+  };
+  replaceGoogleSearch.enable = true;
+  revealAllSpoilers.enable = true;
+  reverseImageSearch.enable = true;
+  sendTimestamps = {
+    enable = true;
+    replaceMessageContents = true;
+  };
+  shikiCodeblocks.enable = true;
+  showConnections.enable = true;
+  showHiddenThings.enable = true;
+  silentTyping = {
+    enable = true;
+    chatContextMenu = true;
+    chatIcon = true;
+    defaultHidden = true;
+  };
+  spotifyCrack = {
+    enable = true;
+    noSpotifyAutoPause = true;
+  };
+  userVoiceShow = {
+    showInMemberList = true;
+    showInMessages = true;
+    showInUserProfileModal = true;
+  };
+  validReply.enable = true;
+  validUser.enable = true;
+  viewIcons.enable = true;
+  voiceChatDoubleClick.enable = true;
+  voiceDownload.enable = true;
+  voiceMessages = {
+    enable = true;
+    echoCancellation = true;
+    noiseSuppression = true;
+  };
+  volumeBooster = {
+    enable = true;
+    multiplier = 2.0;
+  };
+  youtubeAdblock.enable = true;
+}
