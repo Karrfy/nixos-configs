1046532601a36591cdaae2b85cb330b7f02d17f7
Jannis <git@jannis.one>
2026-06-04 13:29:37 +0200

    feat(gaming): add dwproton to steam

--- src/config/modules/gaming/steam.nix
+++ src/config/modules/gaming/steam.nix
@@ -23,6 +23,7 @@
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
       extraCompatPackages = with pkgs; [
+        dwproton-bin
         proton-ge-bin
       ];
     };
